8b7542b3600c4480b379ab4c76abf823075ce1b3
Kal-El Carvalho <kalcarvalho@gmail.com>
2022-08-19 12:18:51 -0300

    Update compras.cfg.php

--- persistence/compras.cfg.php
+++ persistence/compras.cfg.php
@@ -1,16 +1,5 @@
 ﻿<?php
 
- // define("USER_BD",'sysfactor03');
- // define("HOST_BD",'mysql.sysfactor.com.br');
- // define("NAME_BD",'sysfactor03');
- // define("PASSWORD_BD", 'h0u9t6');
- 
- // define("USER_BD",'root');
- // define("HOST_BD",'192.168.0.200');
- // define("NAME_BD",'ctrlmat');
- // define("PASSWORD_BD", '');
- 
-
 define("USER_BD",'root');
 define("HOST_BD",'192.168.0.108');
 define("NAME_BD",'ctrlmat');
